2ae27ce6548a70dcb5b87ff70c6909d5a05c01d8
Ohtico <viticogelvis5@gmail.com>
2021-08-09 12:01:58 -0500

    victor

--- data/data.js
+++ data/data.js
@@ -3,32 +3,32 @@ export const data =
     {
         "id": 1,
         "name": "Yoda",
-        "image": "/images/1.jpg"
+        "image": "https://i.pinimg.com/236x/c2/80/17/c280178fbd75fced99586e3d9d0d3209.jpg"
     },
     {
         "id": 2,
         "name": "Stormtrooper",
-        "image": "/images/2.jpg"
+        "image": "https://i.pinimg.com/236x/a3/28/7f/a3287f999f542133b4378747f08b38b5.jpg"
     },
     {
         "id": 3,
         "name": "R2-D2 & C-3PO",
-        "image": "/images/3.jpg"
+        "image": "https://i.pinimg.com/236x/68/0f/20/680f208d9dece3602225b7c8c99eb179.jpg"
     },
     {
         "id": 4,
         "name": "Darth Vader",
-        "image": "/images/4.jpg"
+        "image": "https://i.pinimg.com/236x/e0/18/8a/e0188a7a7df64c61befe95622ad72441.jpg"
     },
     {
         "id": 5,
         "name": "Obi-Wan Kenobi",
-        "image": "/images/5.jpg"
+        "image": "https://i.pinimg.com/236x/76/d6/82/76d6827ec9506f8298002a753022ecd9.jpg"
     }
     ,
     {
         "id": 6,
         "name": "Chewbacca",
-        "image": "/images/6.jpg"
+        "image": "https://i.pinimg.com/236x/cd/1b/11/cd1b11512fb404fbd400c086598b5efd.jpg"
     }
 ]
