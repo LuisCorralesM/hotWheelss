685fc6e0d105b65d7ea76c4acac461e3cf664a26
David Herrera <biggiegun@gmail.com>
2021-08-09 12:38:03 -0500

    Modificación data.js

--- data/data.js
+++ data/data.js
@@ -2,33 +2,33 @@ export const data =
 [
     {
         "id": 1,
-        "name": "Yoda",
+        "name": "Ford Mustang",
         "image": "https://i.pinimg.com/236x/c2/80/17/c280178fbd75fced99586e3d9d0d3209.jpg"
     },
     {
         "id": 2,
-        "name": "Stormtrooper",
+        "name": "Maserati",
         "image": "https://i.pinimg.com/236x/a3/28/7f/a3287f999f542133b4378747f08b38b5.jpg"
     },
     {
         "id": 3,
-        "name": "R2-D2 & C-3PO",
+        "name": "Ferrari",
         "image": "https://i.pinimg.com/236x/68/0f/20/680f208d9dece3602225b7c8c99eb179.jpg"
     },
     {
         "id": 4,
-        "name": "Darth Vader",
+        "name": "Hyundai Shark",
         "image": "https://i.pinimg.com/236x/e0/18/8a/e0188a7a7df64c61befe95622ad72441.jpg"
     },
     {
         "id": 5,
-        "name": "Obi-Wan Kenobi",
+        "name": "Mazda",
         "image": "https://i.pinimg.com/236x/76/d6/82/76d6827ec9506f8298002a753022ecd9.jpg"
     }
     ,
     {
         "id": 6,
-        "name": "Chewbacca",
+        "name": "For Titanium",
         "image": "https://i.pinimg.com/236x/cd/1b/11/cd1b11512fb404fbd400c086598b5efd.jpg"
     }
 ]
